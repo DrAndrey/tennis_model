fa76bc0dc922b40987f34f9ebd988eb8dac5743f
DAndrey <andreydruzhaev@gmail.com>
2016-08-15 17:57:45 +0300

    Compare tournament name, add players to db

--- db_convertor/db_convertor.py
+++ db_convertor/db_convertor.py
@@ -34,9 +34,9 @@ class Player(Base):
     __tablename__ = 'players'
     player_id = Column(Integer, primary_key=True)
     first_name = Column(String)
-    last_name = Column(String)
+    second_name = Column(String)
     matches = relationship("Match", secondary=player_match_table)
-    __table_args__ = (UniqueConstraint('first_name', 'last_name', name='first_last_names'),)
+    __table_args__ = (UniqueConstraint('first_name', 'second_name', name='first_second_name'),)
 
 
 class Match(Base):
@@ -85,6 +85,97 @@ def load_data(converted_year):
     return atp_df, co_uk_df
 
 
+def compare_tournament_names(atp_df, co_uk_df):
+    atp_df["tournament"] = atp_df["tournament"].apply(lambda x: x.replace("-", " "))
+    co_uk_df["Tournament"] = co_uk_df["Tournament"].apply(lambda x: x.replace(".", "").lower())
+
+    atp_names = atp_df["tournament"].unique()
+    co_uk_names = co_uk_df["Tournament"].unique()
+
+    compared_names = {}
+    search_names = set(atp_names)
+    for atp_name in atp_names:
+        if atp_name in co_uk_names:
+            compared_names[atp_name] = atp_name
+            search_names.remove(atp_name)
+
+    handle_comparison = {"synsam swedish open": "skistar swedish open", "rogers masters": "rogers cup",
+                         "open de tenis comunidad valenciana": "valencia open", "madrid masters": "mutua madrid open",
+                         "qatar exxon mobil open": "qatar exxonmobil open", "estoril open": "portugal open",
+                         "us men's clay court championships": "fayez sarofim  co us mens clay court championship",
+                         "campionati internazional d'italia": "internazionali bnl ditalia",
+                         "open romania": "brd nastase tiriac trophy", "red letter days open": "aegon open nottingham",
+                         "western & southern financial group masters": "western  southern open",
+                         "french open": "roland garros", "davidoff swiss indoors": "swiss indoors basel",
+                         "heineken open": "asb classic", "hamburg tms": "german tennis championships 2016",
+                         "ba-ca tennis trophy": "erste bank open 500", "copa telmex": "argentina open",
+                         "kingfisher airlines tennis open": "mumbai open", "idea prokom open": "orange warsaw open",
+                         "nasdaq-100 open": "miami open presented by itau", "movistar open": "royal guard open chile",
+                         "countrywide classic": "farmers classic", "legg mason classic": "citi open",
+                         "allianz suisse open": "j safra sarasin swiss open gstaad", "ordina open": "ricoh open",
+                         "international championships": "delray beach open", "dutch open": "dutch open tennis",
+                         "regions morgan keegan championships": "memphis open", "channel open": "tennis channel open",
+                         "open seat godo": "barcelona open bancsabadell", "stella artois": "aegon championships",
+                         "rca championships": "indianapolis tennis championships", "open 13": "open 13 provence",
+                         "monte carlo masters": "monte carlo rolex masters", "bmw open": "bmw open by fwu ag",
+                         "hall of fame championships": "hall of fame tennis championships",
+                         "abierto mexicano": "abierto mexicano telcel", "croatia open": "konzum croatia open umag",
+                         "japan open": "rakuten japan open tennis championships 2016",
+                         "grand prix de lyon": "grand prix de tennis de lyon", "open de moselle": "moselle open",
+                         "sydney international": "apia international sydney", "stockholm open": "if stockholm open",
+                         "dubai tennis championships": "dubai duty free tennis championships",
+                         "kremlin cup": "vtb kremlin cup", "chennai open": "aircel chennai open",
+                         "bnp paribas": "bnp paribas masters", "pacific life open": "bnp paribas open"}
+    for co_uk_name, atp_name in handle_comparison.items():
+        compared_names[co_uk_name] = atp_name
+        search_names.remove(atp_name)
+
+    if len(search_names):
+        print(len(search_names))
+        print(search_names)
+        print(set(co_uk_names) - compared_names.keys())
+        raise Exception("There is more tournament names to search")
+
+    co_uk_df["Tournament"] = co_uk_df["Tournament"].map(compared_names)
+
+
+def create_players(session, atp_df):
+
+    def determine_long_name(splitted_name):
+        articles = ["de", "el", "di", "del", "van", "der"]
+        for border_num, name_part in enumerate(splitted_name[1:]):
+            if name_part in articles:
+                break
+        border_num += 1
+        first_name = " ".join(splitted_name[: border_num])
+        second_name = " ".join(splitted_name[border_num:])
+
+        if first_name == "martin vassallo":
+            first_name = "martin"
+            second_name = "vassallo-arguello"
+        return first_name, second_name
+
+    winner_full_name = atp_df.apply(lambda x: "{0} {1}".format(x["winner_first_name"], x["winner_second_name"]), axis=1)
+    loser_full_name = atp_df.apply(lambda x: "{0} {1}".format(x["loser_first_name"], x["loser_second_name"]), axis=1)
+
+    unique_winner_names = set(winner_full_name.unique())
+    unique_loser_names = set(loser_full_name.unique())
+    unique_names = unique_winner_names.union(unique_loser_names)
+
+    for name in unique_names:
+        clean_name = name.lower().replace("jr.", "")
+        splitted_name = clean_name.split()
+        if len(splitted_name) == 2:
+            first_name = splitted_name[0]
+            second_name = splitted_name[1]
+        else:
+            first_name, second_name = determine_long_name(splitted_name)
+        if not session.query(Player).filter(first_name == first_name and second_name == second_name).count():
+            player = Player(first_name=first_name, second_name=second_name)
+            session.add(player)
+    session.commit()
+
+
 if __name__ == '__main__':
     converted_year = 2006
     engine = create_engine('sqlite:///tennis_model.db')
@@ -97,3 +188,9 @@ if __name__ == '__main__':
     init_games(session)
 
     atp_df, co_uk_df = load_data(converted_year)
+
+    print(atp_df.columns.values)
+    print(co_uk_df.columns.values)
+
+    compare_tournament_names(atp_df, co_uk_df)
+    create_players(session, atp_df)
